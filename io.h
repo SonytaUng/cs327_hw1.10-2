5bf0192bd2d57d2fc88b5d5fc01a99d79f50a926
Sonyta Ung <50857832+SonytaUng@users.noreply.github.com>
2021-04-28 22:45:38 -0500

    Add files via upload

---
+++ io.h
@@ -0,0 +1,15 @@
+#ifndef IO_H
+# define IO_H
+
+class dungeon;
+
+void io_init_terminal(void);
+void io_reset_terminal(void);
+void io_display(dungeon *d);
+void io_display_no_fog(dungeon *d);
+void spells(dungeon *d);
+void pickup_book(dungeon *d);
+void io_handle_input(dungeon *d);
+void io_queue_message(const char *format, ...);
+
+#endif
